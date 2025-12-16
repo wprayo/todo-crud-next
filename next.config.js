7f2d16ba1c56f7d56902f6d74526f17d6f81c7e7
wprayo <mdwiprayoga10@gmail.com>
2025-12-16 15:05:52 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
+  // Server Actions sudah enabled by default di Next.js 14
+  // Tidak perlu config apapun
 }
 
 module.exports = nextConfig
