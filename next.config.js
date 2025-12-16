22aaaabfdd9ec14d78a5ab3358616cbe97ae0cc2
wprayo <mdwiprayoga10@gmail.com>
2025-12-16 15:09:02 +0700

    Remove comments and simplify next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Server Actions sudah enabled by default di Next.js 14
-  // Tidak perlu config apapun
-}
+const nextConfig = {}
 
 module.exports = nextConfig
